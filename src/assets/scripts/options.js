38602aa41ce7d987b6899eb2b0969d935479178f
Carles Muiños <carles.ml.dev@gmail.com>
2017-11-30 13:42:14 +0100

    configure date picker

--- src/assets/scripts/options.js
+++ src/assets/scripts/options.js
@@ -1,5 +1,41 @@
 window.mr = {
-  smoothscroll:{
+  datepicker: {
+    options: {
+      monthsFull: [
+        'Enero',
+        'Febrero',
+        'Marzo',
+        'Abril',
+        'Mayo',
+        'Junio',
+        'Julio',
+        'Agosto',
+        'Septiembre',
+        'Octubre',
+        'Noviembre',
+        'Diciembre'
+      ],
+      weekdaysShort: [
+        'Do',
+        'Lu',
+        'Ma',
+        'Mi',
+        'Ju',
+        'Vi',
+        'Sa'
+      ],
+      today: 'Hoy',
+      clear: 'Borrar',
+      close: 'Cerrar',
+      labelMonthNext: 'Mes siguiente',
+      labelMonthPrev: 'Mes anterior',
+      labelMonthSelect: 'Selecciona un mes',
+      labelYearSelect: 'Selecciona un año',
+      firstDay: 1,
+      max: true
+    }
+  },
+  smoothscroll: {
     options: {
         easing: 'swing',
         speed: 400
